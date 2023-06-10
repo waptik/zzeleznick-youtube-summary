05d4856ce029a1f858afb038d51eed1bee5a106a
Zach Zeleznick <zzeleznick@gmail.com>
2023-06-10 10:43:54 -0700

    wip: think about using break words as creating as tokens but seems brittle and too context dependent

--- extra.ts
+++ extra.ts
@@ -4,28 +4,57 @@ import registry from "https://esm.sh/@dqbd/tiktoken@1.0.7/registry.json" assert 
 import models from "https://esm.sh/@dqbd/tiktoken@1.0.7/model_to_encoding.json" assert { type: "json" };
 
 type ObjectValues<T> = T[keyof T];
-type Registry = Record<ObjectValues<typeof models>, ObjectValues<typeof registry>>;
+type Model = keyof typeof models
+type Encoder = ObjectValues<typeof models>
+type EncoderConfig = ObjectValues<typeof registry>
+type Registry = Record<Encoder, EncoderConfig>;
 
-async function tokenize(text: string) {
-    // Initialize the wasm via discussion in https://github.com/dqbd/tiktoken/issues/22
-    await init(async (imports) => {
-        const req = await fetch('https://esm.sh/@dqbd/tiktoken@1.0.7/lite/tiktoken_bg.wasm')
-        return WebAssembly.instantiate(await req.arrayBuffer(), imports)
-    });
+// TODO: support other languages
+const BreakWords = [
+    "and",
+    "but",
+    "however",
+    "also",
+    "so",
+    "then",
+] as const;
+
+let encoder: Tiktoken;
+let breakWordTokens: Set<number>;
+
+async function tokenize(text: string, modelName?: Model) {
+    if (!encoder) {
+        // Initialize the wasm via discussion in https://github.com/dqbd/tiktoken/issues/22
+        await init(async (imports) => {
+            const req = await fetch('https://esm.sh/@dqbd/tiktoken@1.0.7/lite/tiktoken_bg.wasm')
+            return WebAssembly.instantiate(await req.arrayBuffer(), imports)
+        });
+    }
+    modelName = modelName ?? "gpt-3.5-turbo"
     // MARK: gpt-3.5-turbo uses the cl100k_base encoding whereas text-davinci-003 uses the p50k_base
-    const model = await load((registry as Registry)[models["gpt-3.5-turbo"]]);
-    const encoding = new Tiktoken(
+    const model = await load((registry as Registry)[models[modelName]]);
+    encoder = new Tiktoken(
         model.bpe_ranks,
         model.special_tokens,
         model.pat_str
     );
-    const tokens = encoding.encode(text);
-    encoding.free();
+    const tokens = encoder.encode(text);
+    encoder.free();
     return tokens;
 }
 
+async function tokenizeFillerWords() {
+    if (!breakWordTokens) {
+        const tokens = await tokenize(BreakWords.join(" "));
+        console.log(tokens);
+    }
+}
+
 if (import.meta.main) {
-    const tokens = await tokenize("hello world i'm a teapot");
-    console.log(tokens);
-    console.log(`Token count: ${tokens.length}`);
+    // const text = "hello world i'm a teapot";
+    // const text = await Deno.readTextFile("./snyk2.log");
+    // const tokens = await tokenize(text);
+    // console.log(tokens);
+    // console.log(`Token count: ${tokens.length}`);
+    await tokenizeFillerWords();
 }
