ce8826119d50816299cf70c9664e1c560e4555a1
Stephane Mensah <waptik@users.noreply.github.com>
2023-09-18 01:52:34 +0000

    json response

--- main.ts
+++ main.ts
@@ -4,7 +4,12 @@ if (import.meta.main) {
   Deno.serve(async (req: Request) => {
     const url = "https://www.youtube.com/watch?v=46IEp7_mpdw";
     const transcript = await YoutubeTranscript.fetchTranscript(url);
-
-    return new Response(transcript);
+    const body = JSON.stringify(transcript);
+    
+    return new Response(body, {
+    headers: {
+      "content-type": "application/json; charset=utf-8",
+    },
+  });
   });
 }
